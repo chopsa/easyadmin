4ebd752023b3f58cc86ab613982ac157a0caab6e
Semisi Topui <smtopui@gmail.com>
2023-05-27 09:30:15 +1300

    completed ch10

--- src/Controller/Admin/UserCrudController.php
+++ src/Controller/Admin/UserCrudController.php
@@ -7,8 +7,12 @@ use EasyCorp\Bundle\EasyAdminBundle\Field\Field;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ArrayField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\EmailField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 
 class UserCrudController extends AbstractCrudController
@@ -20,11 +24,29 @@ class UserCrudController extends AbstractCrudController
 
     public function configureFields(string $pageName): iterable
     {
-        yield IdField::new('id');
+        yield IdField::new('id')
+            ->onlyOnIndex();
+        yield ImageField::new('avatar')
+            ->setBasePath('uploads/avatars')
+            ->setUploadDir('public/uploads/avatars')
+            ->setUploadedFileNamePattern('[slug]-[timestamp].[extension]');
         yield EmailField::new('email');
-        yield TextField::new('fullName');
+        yield TextField::new('fullName')
+            // ->onlyOnIndex();
+            ->hideOnForm();
+        yield Field::new('firstName')
+            ->onlyOnForms();
+        yield Field::new('lastName')
+            ->onlyOnForms();
         yield BooleanField::new('enabled')
             ->renderAsSwitch(false);
-        yield DateField::new('createdAt'); // we don't see createdAt in the User entity list but it's because it lives in the TimeStampableEntity trait
+        yield DateField::new('createdAt') // we don't see createdAt in the User entity list but it's because it lives in the TimeStampableEntity trait
+            ->hideOnForm();
+        $roles = ['ROLE_SUPER_ADMIN', 'ROLE_ADMIN', 'ROLE_MODERATOR', 'ROLE_USER'];
+        yield ChoiceField::new('roles')
+            ->setChoices(array_combine($roles, $roles))
+            ->allowMultipleChoices()
+            ->renderExpanded()
+            ->renderAsBadges();
     }
 }
