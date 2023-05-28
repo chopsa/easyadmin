961a9c07241bd2a3e39f002b14f4abfeebd20826
Semisi Topui <smtopui@gmail.com>
2023-05-28 19:23:31 +1300

    completed ch24

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -124,7 +124,8 @@ class DashboardController extends AbstractDashboardController
         return parent::configureCrud()
             ->setDefaultSort([
                 'id' => 'DESC',
-            ]);
+            ])
+            ->overrideTemplate('crud/field/id', 'admin/field/id_with_icon.html.twig');
     }
 
     // private function createChart(): Chart
